2bc222e1d757fbf345b2af637fd7167da627ea00
Richard Rosenberg <rosenbr73@gmail.com>
2022-01-20 15:24:57 -0800

    JS set up

--- calc.js
+++ calc.js
@@ -1,1 +1,15 @@
-// function that takes 3 arguments, number 1, number 2, and operator
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOpTextElement = document.querySelector('[data-previous-op]');
+const currentOpTextElement = document.querySelector('[data-current-op]');
+
+class Calculator {
+    constructor(previousOpTextElement, currentOpTextElement) {
+        this.previousOpTextElement = previousOpTextElement
+        this.currentOpTextElement = currentOpTextElement
+        this.clear()
+    }
+}
